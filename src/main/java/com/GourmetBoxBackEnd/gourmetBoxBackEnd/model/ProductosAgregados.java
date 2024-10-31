37bff0a875efbb4cfdb1dd05fb73a412a7588c31
3duard89 <dduardidrobo@genstudents.org>
2024-10-31 12:39:51 -0500

    Arreglando los nombres

--- src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/model/ProductosAgregados.java
+++ src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/model/ProductosAgregados.java
@@ -11,19 +11,19 @@ import lombok.Setter;
 public class ProductosAgregados {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer idProductoAgregado;
+        private Integer idProductosAgregado;
 
         @Column(nullable = false)
-        private Integer idUsuario;
+        private Integer usuarioId;
 
         @Column(nullable = false)
-        private Integer idProducto;
+        private Integer productoId;
 
         @Column(nullable = false)
         private Integer cantidadProducto;
 
         @Column(nullable = false)
-        private Integer idResumenCompra;
+        private Integer resumenCompraId;
 
         @Column(nullable = false)
         private Double subtotal;
@@ -31,10 +31,10 @@ public class ProductosAgregados {
         public ProductosAgregados() {}
 
         public ProductosAgregados(Integer idProductoAgregado, Integer usuario, Integer producto, Integer idResumenCompra,Integer cantidadProducto, Double subtotal) {
-            this.idProductoAgregado = idProductoAgregado;
-            this.idUsuario = usuario;
-            this.idProducto = producto;
-            this.idResumenCompra = idResumenCompra;
+            this.idProductosAgregado = idProductoAgregado;
+            this.usuarioId = usuario;
+            this.productoId = producto;
+            this.resumenCompraId = idResumenCompra;
             this.cantidadProducto = cantidadProducto;
             this.subtotal = subtotal;
         }
