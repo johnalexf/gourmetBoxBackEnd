83e72691cc2fe6fdc38f7cc08d8dbfc59dc62540
Rafael Arteaga <arteagarafat@gmail.com>
2024-11-02 12:38:39 -0500

    Consultas realizadas a producto y resumen compra

--- src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/model/ResumenCompra.java
+++ src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/model/ResumenCompra.java
@@ -1,5 +1,6 @@
 package com.GourmetBoxBackEnd.gourmetBoxBackEnd.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,6 +28,7 @@ public class ResumenCompra {
 
 
     @OneToMany(mappedBy = "resumenCompra")
+    @JsonIgnore
     private List<ProductosAgregados> productosAgregados;
 
     public ResumenCompra() {
