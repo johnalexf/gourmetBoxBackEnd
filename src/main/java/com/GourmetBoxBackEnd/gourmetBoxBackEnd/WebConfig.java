fb7344d2285db802692565aceffc395c93fbc966
John Forero <johnalexfr@gmail.com>
2024-11-07 15:17:33 -0500

    modificando url de enlace con pagina front desplegada

--- src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/WebConfig.java
+++ src/main/java/com/GourmetBoxBackEnd/gourmetBoxBackEnd/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5502")
+                //.allowedOrigins("http://127.0.0.1:5502")
+                .allowedOrigins("https://ss2024-gen.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
